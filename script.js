f9eee64b6f48f9db3e98e9a6803df3338c6af328
torihaku <jatzibui@gmail.com>
2024-03-20 08:42:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -162,4 +162,33 @@ $(document).ready(function(){
         $("#modal").addClass("hidden");
         $("body").removeClass("overflow-hidden");
     });
+
+    // Asetetaan oletusteema latauksen yhteydessä
+    const currentTheme = localStorage.getItem('torihakutheme') ? localStorage.getItem('torihakutheme') : null;
+    if (currentTheme) {
+        document.documentElement.classList.add(currentTheme);
+        if (currentTheme === 'dark') {
+            $('#themeToggle span').text('dark_mode');
+        } else {
+            $('#themeToggle span').text('light_mode');
+        }
+    } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.classList.add('dark');
+        $('#themeToggle span').text('dark_mode');
+    }
+
+    // Käsittele teemanvaihto
+    $('#themeToggle button').click(function() {
+        var isDarkMode = document.documentElement.classList.contains('dark');
+
+        if (isDarkMode) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('torihakutheme', 'light');
+            $('#themeToggle span').text('light_mode');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('torihakutheme', 'dark');
+            $('#themeToggle span').text('dark_mode');
+        }
+    });
 });
